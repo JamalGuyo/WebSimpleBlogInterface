bb2f71d79ffe07f01fc7c64672b78c161c0e4098
Jamal Guyo <jamalguyo08@gmail.com>
2018-03-03 15:24:29 +0300

    original commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8"/>
+        <link rel="stylesheet" type="text/css" href="style.css"/>
+        <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,900" rel="stylesheet">
+    </head>
+<body>
+<!--    FIRST ARTICLE-->
+    <div class="post">    
+        <div class="date">November 23 2015</div>
+        <h2>This is my first article</h2>
+        <p class="quote">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+        </p>
+        <p>
+            Elit sed vulputate mi sit. Tempus egestas sed sed risus pretium quam vulputate dignissim. Habitant morbi tristique senectus et netus. Erat nam at lectus urna duis convallis. Quam quisque id diam vel quam elementum. Sit amet risus nullam eget. Consectetur libero id faucibus nisl tincidunt eget nullam non. Nibh sed pulvinar proin gravida.
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Habitant morbi tristique senectus et netus et malesuada. 
+        </p>
+        <hr>
+    </div>
+    <!--        -->
+    <!--    SECOND ARTICLE-->
+    <div class="post">
+        <div class="date">December 11 2015</div>
+        <h2>This is Another article</h2>
+        <p class="quote"> 
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+        </p>
+        <p>
+            Elit sed vulputate mi sit. Tempus egestas sed sed risus pretium quam vulputate dignissim. Habitant morbi tristique senectus et netus. Erat nam at lectus urna duis convallis. Quam quisque id diam vel quam elementum. Sit amet risus nullam eget. Consectetur libero id faucibus nisl tincidunt eget nullam non. Nibh sed pulvinar proin gravida.
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Habitant morbi tristique senectus et netus et malesuada. 
+        </p>
+    </div>
+</body>    
+</html>
